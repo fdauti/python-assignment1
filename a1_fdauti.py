dfba0d945cf92a08075b993f7cdfdb0d6b6772fd
Fatjon Dauti <31435628+fdauti@users.noreply.github.com>
2021-02-21 00:59:22 -0500

    added rerefence link to the leap year algorithm

--- a1_fdauti.py
+++ a1_fdauti.py
@@ -21,7 +21,7 @@ import sys
 
 def leap_year(obj):
     '''Check to see if the year entered by the user is a leap year'''
-    # Leap year algorithm 
+    # https://en.wikipedia.org/wiki/Leap_year#Algorithm
     # if (year is not divisible by 4) then (it is a common year)
     # else if (year is not divisible by 100) then (it is a leap year)
     # else if (year is not divisible by 400) then (it is a common year)
